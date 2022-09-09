7a479332222c28b570f450fcffd8cac760907ae6
Khushi <khushi.kavya6910@gmail.com>
2022-09-10 00:25:23 +0530

    Markdown-editor

--- public/app.js
+++ public/app.js
@@ -1,20 +1,10 @@
 const textEditor = document.querySelector(".text-editor");
 const preview = document.querySelector(".preview");
-const { marked } = require('marked');
-const renderPreview = (value) => {
-    const html = marked.parse('# Marked in Node.js\n\nRendered by **marked**.');
-    document.getElementById('text-editor').innerHTML =
-      marked.parse('# Marked in browser\n\nRendered by **marked**.');
-    preview.innerText = html;
-    console.log(html);
+
+function converter(){
+    let te = textEditor.value;
+    let prev = marked.parse(te);
+    preview.innerHTML=prev;
 }
 
-// console.log('preview',preview);
-// console.log('text editor',textEditor);
-
-textEditor.addEventListener("keyup", evt => {
-    const {value} = evt.target;
-    console.log(value);
-    renderPreview(value)
-    // console.log(value);
-});
+textEditor.addEventListener('input',converter);
